dbd9cf4c86da8754ccafe023d4c4f1711aa729e3
IsaacVerm <isaacverm@gmail.com>
2025-10-22 15:49:42 +0000

    fix bug empty last line because extra clock time for each game

--- src/moves.sh
+++ src/moves.sh
@@ -1,8 +1,12 @@
+# the last element in clocks might contain the time of resigning which we don't care about
+# id has to be variable because if not you try to access index of the loop in $repeated_id so 0.id instead of json.id
+
 jq -r '
-  .id as $id # id has to be variable because if not you try to access index of the loop in $repeated_id so 0.id instead of json.id
+  .id as $id 
   | (.moves | split(" ")) as $moves
+  | (.clocks[:-1]) as $clocks 
   | ([range(0, $moves | length) | $id]) as $repeated_id
   | (.analysis | map(.eval)) as $evals
-  | [$repeated_id, $moves, .clocks, $evals] | transpose[]
+  | [$repeated_id, $moves, $clocks, $evals] | transpose[]
   | @csv
 ' data/games.ndjson > data/moves.csv
