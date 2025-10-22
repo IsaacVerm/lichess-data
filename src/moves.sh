01e12016bc8b6f7fdd87ce1d010c2fb3b24d692a
IsaacVerm <isaacverm@gmail.com>
2025-10-22 16:41:47 +0000

    add header to moves

--- src/moves.sh
+++ src/moves.sh
@@ -1,6 +1,8 @@
+# create header
+echo 'game_id,move,clock,eval,blunder' > data/moves.csv
+
 # the last element in clocks might contain the time of resigning which we don't care about
 # id has to be variable because if not you try to access index of the loop in $repeated_id so 0.id instead of json.id
-
 jq -r '
   .id as $id 
   | (.moves | split(" ")) as $moves
@@ -10,4 +12,4 @@ jq -r '
   | (.analysis | map(if .judgment.name == "Blunder" then 1 else 0 end)) as $blunders
   | [$repeated_id, $moves, $clocks, $evals, $blunders] | transpose[]
   | @csv
-' data/games.ndjson > data/moves.csv
+' data/games.ndjson >> data/moves.csv
