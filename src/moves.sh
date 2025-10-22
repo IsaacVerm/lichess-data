51e2e756cd575de9e03abc5c75065245b9b66a59
IsaacVerm <isaacverm@gmail.com>
2025-10-22 11:33:37 +0000

    add game_id to moves

--- src/moves.sh
+++ src/moves.sh
@@ -1,6 +1,8 @@
 jq -r '
-  (.moves | split(" ")) as $moves
+  .id as $id # id has to be variable because if not you try to access index of the loop in $repeated_id so 0.id instead of json.id
+  | (.moves | split(" ")) as $moves
+  | ([range(0, $moves | length) | $id]) as $repeated_id
   | (.analysis | map(.eval)) as $evals
-  | [$moves, .clocks, $evals] | transpose[]
+  | [$repeated_id, $moves, .clocks, $evals] | transpose[]
   | @csv
 ' data/games_example.ndjson > data/moves.csv
