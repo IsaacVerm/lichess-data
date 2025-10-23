18171952af8e67db9d9d0b08c0013ae614e869a7
IsaacVerm <isaacverm@gmail.com>
2025-10-23 20:14:26 +0000

    reduce example data + use loop instead of transpose to create csv row
    Using the jq transpose function was too much in the end.
    To set the end_opening I had to change elements in an array already mapped.
    All of this can be avoided by just using a loop.
    No premature optimization needed.

--- src/moves.sh
+++ src/moves.sh
@@ -1,5 +1,5 @@
 # create header
-echo 'game_id,ply,move,clock,eval,blunder,end_opening' > data/moves.csv
+echo 'game_id,ply,move,clock,eval,type_of_mistake,end_opening' > data/moves.csv
 
 # the last element in clocks might contain the time of resigning which we don't care about
 # id has to be variable because if not you try to access index of the loop in $repeated_id so 0.id instead of json.id
@@ -7,17 +7,14 @@ jq -r '
   .id as $id
   | (.division.middle) as $end_opening_ply
   | (.moves | split(" ")) as $moves
-  | (.clocks[:-1]) as $clocks
-  | (.analysis | map(.eval)) as $evals
-  | (.analysis | map(if .judgment.name == "Blunder" then 1 else 0 end)) as $blunders
   | range(0; $moves | length) as $i
   | [
       $id,
       $i+1,
       $moves[$i],
-      $clocks[$i],
-      $evals[$i],
-      $blunders[$i],
+      .clocks[$i],
+      .analysis[$i].eval,
+      .analysis[$i].judgment.name,
       (if $i == $end_opening_ply then 1 else 0 end)
     ]
   | @csv
