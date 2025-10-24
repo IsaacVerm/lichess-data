5726c593bece686e773d04998d44ea212bed10da
IsaacVerm <isaacverm@gmail.com>
2025-10-24 14:51:40 +0000

    add url_ply to moves

--- src/moves.sh
+++ src/moves.sh
@@ -1,5 +1,5 @@
 # create header
-echo 'game_id,ply,move,clock,eval,type_of_mistake,end_opening' > data/moves.csv
+echo 'game_id,ply,url_ply,move,clock,eval,type_of_mistake,end_opening' > data/moves.csv
 
 # the last element in clocks might contain the time of resigning which we don't care about
 # id has to be variable because if not you try to access index of the loop in $repeated_id so 0.id instead of json.id
@@ -11,6 +11,7 @@ jq -r '
   | [
       $id,
       $i+1,
+      "https://lichess.org/" + $id + "#" + ($i+1 | tostring),
       $moves[$i],
       .clocks[$i],
       .analysis[$i].eval,
