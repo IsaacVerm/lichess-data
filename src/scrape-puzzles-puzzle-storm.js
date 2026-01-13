65d9fbfe9e73add2b5bf122b65b9878767db006a
IsaacVerm <isaacverm@gmail.com>
2026-01-13 22:04:35 +0100

    Use a single runId for all rounds

--- src/scrape-puzzles-puzzle-storm.js
+++ src/scrape-puzzles-puzzle-storm.js
@@ -1,8 +1,10 @@
+const runId = crypto.randomUUID();
+
 JSON.stringify(
   Array.from(document.querySelectorAll('.puz-history__round')).map(round => {
     const resultElement = round.querySelector('good') || round.querySelector('bad');
     return {
-      run_id: crypto.randomUUID(),
+      run_id: runId,
       rating: parseInt(round. querySelector('rating').textContent),
       href: round.querySelector('a').getAttribute('href'),
       result: round.querySelector('good') ? 'good' : 'bad',
