921a5e1ecadec785b0d1f7e06319dfb9e2077109
IsaacVerm <isaacverm@gmail.com>
2026-02-09 08:52:49 +0100

    update field names stats-first-fail-by-date

--- src/agg/stats-first-fail-by-date.sql
+++ src/agg/stats-first-fail-by-date.sql
@@ -1,13 +1,13 @@
 select
   date,
   count(distinct run_id) as count_runs,
-  min(rank_in_run) as min_rank_in_run_first_fail,
-  max(rank_in_run) as max_rank_in_run_first_fail,
+  min(rank_in_run) as min_rank_first_fail,
+  max(rank_in_run) as max_score,
   case
     when count(distinct run_id) > 1
     then max(rank_in_run) - min(rank_in_run)
     else NULL
-    end as range_first_fail,
+    end as deviation_early_fail_high_score,
   round(avg(rank_in_run), 2) as avg_rank_in_run_first_fail
 from
   enrich_puzzles_puzzle_storm
