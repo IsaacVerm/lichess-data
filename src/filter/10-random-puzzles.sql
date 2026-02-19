e1792aa124f398b95ce6980c029e0d84a81f85cb
IsaacVerm <isaacverm@gmail.com>
2026-02-19 07:49:33 +0100

    update src/10-random-puzzles.sql

--- src/filter/10-random-puzzles.sql
+++ src/filter/10-random-puzzles.sql
@@ -1,4 +1,5 @@
 select url, date, rating, clock, rank_run
 from enrich_puzzles_puzzle_storm
+where result = 'bad'
 order by ceiling(random() * (select max(rank_run) from enrich_puzzles_puzzle_storm))
 limit 10
